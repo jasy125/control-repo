02ae8563613b07441707764018f94bfe9811c49e
jasy125 <jas89_44@hotmail.com>
2019-05-08 15:08:55 +0100

    remove

--- modules/test/manifests/cron.pp
+++
@@ -1,6 +0,0 @@
-cron { 'taste_os scan as user': 
-          command => "${destination_dir}/taste_os.sh -c ${destination_dir}/config.sh -p ${destination_dir} -d 120 > /dev/null 2>&1", 
-          hour => $cron_hour, 
-          minute => $cron_minute, 
-          user => $taste_os_user_name, 
-      },
--- modules/test/manifests/init.pp
+++ modules/test/manifests/init.pp
@@ -1,3 +1,8 @@
-class test{
-  include test::cron
+class test () {
+  cron { 'taste_os scan as user': 
+          command => "${destination_dir}/taste_os.sh -c ${destination_dir}/config.sh -p ${destination_dir} -d 120 > /dev/null 2>&1", 
+          hour => $cron_hour, 
+          minute => $cron_minute, 
+          user => $taste_os_user_name, 
+      },
 }
