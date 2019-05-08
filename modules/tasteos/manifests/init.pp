7b4e73224c1d367c02366d8068c2e3028e7bba5d
jasy125 <jas89_44@hotmail.com>
2019-05-08 14:12:03 +0100

    fd

--- modules/tasteos/manifests/init.pp
+++ modules/tasteos/manifests/init.pp
@@ -1,4 +1,4 @@
-cron { 'taste_os scan as user':
+class tasteos { 'taste_os scan as user':
           command => "${destination_dir}/taste_os.sh -c ${destination_dir}/config.sh -p ${destination_dir} -d 120 > /dev/null 2>&1",
           hour => $cron_hour,
           minute => $cron_minute,
