e444ff6595349532dd0689c6f07595aac83d40f0
jasy125 <jas89_44@hotmail.com>
2019-05-08 15:42:54 +0100

    test

--- modules/test/manifests/tasteos.pp
+++ modules/test/manifests/tasteos.pp
@@ -1,21 +1,17 @@
 class test::tasteos {
    exec { 'taste_os register':
-      command   => "${destination_dir}/taste_os.sh -c ${destination_dir}/config.sh --mode reg --path ${destination_dir}",
-      cwd       => $destination_dir,
-      creates   => "${destination_dir}/collect.sh",
-      subscribe => File["${destination_dir}/config.sh"],
-      user      => $taste_os_user_name,
+      user      => "1",
     }
     -> cron { 'taste_os scan as user':
-      command => "${destination_dir}/taste_os.sh -c ${destination_dir}/config.sh -p ${destination_dir} -d 120 > /dev/null 2>&1",
-      hour    => $cron_hour,
-      minute  => $cron_minute,
-      user    => $taste_os_user_name,
+      command => "cron",
+      hour    => "1",
+      minute  => "1",
+      user    => "1",
     }
     cron { 'taste_os scan':
       ensure  => absent,
-      command => "${destination_dir}/taste_os.sh -c ${destination_dir}/config.sh -p ${destination_dir} -d 120 > /dev/null 2>&1",
-      hour    => $cron_hour,
-      minute  => $cron_minute,
+      command => "cron",
+      hour    => "1",
+      minute  => "1",
     }
 }
