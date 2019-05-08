000eba01d6554e1bd2f4fd8e3f384bdbb69b1c83
jasy125 <jas89_44@hotmail.com>
2019-05-08 15:48:35 +0100

    test

--- modules/test/manifests/tasteos.pp
+++ modules/test/manifests/tasteos.pp
@@ -1,8 +1,5 @@
 class test::tasteos {
-   exec { 'taste_os register':
-      user      => "1",
-    }
-    -> cron { 'taste_os scan as user':
+   cron { 'taste_os scan as user':
       command => "cron",
       hour    => "1",
       minute  => "1",
