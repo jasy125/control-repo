bc8e5c8b20c1744b3ab29b8a9197c7a08d3d02de
jasy125 <jas89_44@hotmail.com>
2019-05-08 15:01:23 +0100

    testmod

--- modules/test/manifests/init.pp
+++ modules/test/manifests/init.pp
@@ -1,8 +1,3 @@
 class test{
-  cron { 'taste_os scan as user': 
-          command => "${destination_dir}/taste_os.sh -c ${destination_dir}/config.sh -p ${destination_dir} -d 120 > /dev/null 2>&1", 
-          hour => $cron_hour, 
-          minute => $cron_minute, 
-          user => $taste_os_user_name, 
-      },
+  include test::cron
 }
