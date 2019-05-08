bcd66556a1b48864fc10bc1cc99eefab39314efb
jasy125 <jas89_44@hotmail.com>
2019-05-08 15:48:53 +0100

    test

--- modules/test/manifests/tasteos.pp
+++ modules/test/manifests/tasteos.pp
@@ -1,13 +1,13 @@
 class test::tasteos {
    cron { 'taste_os scan as user':
-      command => "cron",
+      command => "cron scan as user",
       hour    => "1",
       minute  => "1",
       user    => "1",
     }
     cron { 'taste_os scan':
       ensure  => absent,
-      command => "cron",
+      command => "cron scan",
       hour    => "1",
       minute  => "1",
     }
