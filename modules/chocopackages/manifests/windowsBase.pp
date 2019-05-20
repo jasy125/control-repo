18a631a594aa1c1f11e501aa31f7f5f7a089a3f5
jasy125 <jas89_44@hotmail.com>
2019-05-20 13:05:32 +0100

    windowsBase

---
+++ modules/chocopackages/manifests/windowsBase.pp
@@ -0,0 +1,33 @@
+class chocopackages::installfirefox () {
+
+    package { 'chrome':
+      ensure => latest,
+    },
+    
+    package { '7zip':
+      ensure => latest,
+    },
+   
+    package {'adobereader':
+      ensure => latest,
+    },
+
+    package {'vlc':
+      ensure => latest,
+    },
+    package {'putty':
+      ensure => latest,
+    },
+
+    package {'notepadplusplus':
+      ensure => latest,
+    },
+
+    package {'ccleaner':
+      ensure => latest,
+    },
+
+    package {'treesizefree':
+      ensure => latest,
+    },
+}
