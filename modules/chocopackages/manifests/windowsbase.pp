2973b87a9ac5e7b199b6b216320825004cd687c5
jasy125 <jas89_44@hotmail.com>
2019-05-21 11:09:07 +0100

    choco

--- modules/chocopackages/manifests/windowsbase.pp
+++ modules/chocopackages/manifests/windowsbase.pp
@@ -3,32 +3,41 @@ class chocopackages::windowsbase {
     package { 'firefox':
       ensure   => latest,
     }
+
     package { '7zip':
       ensure => latest,
-    },
+    }
+
+    package {'adobereader':
+      ensure => latest,
+    }
+
+    package {'vlc':
+      ensure => latest,
+    }
+
+    package {'putty':
+      ensure => latest,
+    }
+
+    package {'notepadplusplus':
+      ensure => latest,
+    }
+
+    package {'ccleaner':
+      ensure => latest,
+    }
+
+    package {'treesizefree':
+      ensure => latest,
+    }
     
  /*   
     
    package { 'chrome':
       ensure => latest,
     }
-    package {'adobereader':
-      ensure => latest,
-    },
-    package {'vlc':
-      ensure => latest,
-    },
-    package {'putty':
-      ensure => latest,
-    },
-    package {'notepadplusplus':
-      ensure => latest,
-    },
-    package {'ccleaner':
-      ensure => latest,
-    },
-    package {'treesizefree':
-      ensure => latest,
-    },
+ 
+
     */
 }
