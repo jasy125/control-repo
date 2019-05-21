a1f1f5d174facb1224561b84fa93a9060ce5305d
Jas <30461450+jasy125@users.noreply.github.com>
2019-05-21 11:04:16 +0100

    Create windowsbase.pp

---
+++ modules/chocopackages/manifests/windowsbase.pp
@@ -0,0 +1,34 @@
+class chocopackages::windowsbase {
+
+    package { 'firefox':
+      ensure   => latest,
+    }
+
+    package { 'chrome':
+      ensure => latest,
+    }
+ /*   
+    package { '7zip':
+      ensure => latest,
+    },
+   
+    package {'adobereader':
+      ensure => latest,
+    },
+    package {'vlc':
+      ensure => latest,
+    },
+    package {'putty':
+      ensure => latest,
+    },
+    package {'notepadplusplus':
+      ensure => latest,
+    },
+    package {'ccleaner':
+      ensure => latest,
+    },
+    package {'treesizefree':
+      ensure => latest,
+    },
+    */
+}
