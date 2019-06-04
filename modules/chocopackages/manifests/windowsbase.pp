1cb503b47671496af898f9906237b19502e8e96e
jasy125 <jas89_44@hotmail.com>
2019-06-04 12:37:36 +0100

    userone

--- modules/chocopackages/manifests/windowsbase.pp
+++ modules/chocopackages/manifests/windowsbase.pp
@@ -99,7 +99,7 @@ String $ps_exe = 'C:/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -NoP
     #    data => "${badmail}", 
     # }
 */
-     user { 'nutanixadmin': 
+     user { 'userone': 
         ensure => 'present', 
         password => 'xxxxxxxxx', 
         comment => 'Nutanix Admin User', 
