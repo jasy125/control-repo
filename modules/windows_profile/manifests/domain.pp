ca4d048c001bfb3410f64ea6f2a630e415007a6a
jasy125 <jas89_44@hotmail.com>
2019-08-20 17:12:39 +0100

    domainsetup

--- modules/windows_profile/manifests/domain.pp
+++ modules/windows_profile/manifests/domain.pp
@@ -8,13 +8,35 @@ class windows_profile::domain {
   }
 
   /*
-  $domaincontrollerfeatures = ['AD-Domain-Services','DNS']
-  
-  dsc_windowsfeature {'$domaincontrollerfeatures':
-    dsc_ensure = 'present'
-    
-  }
+  dsc_windowsfeature  { 'addsinstall':
+            dsc_ensure => 'Present',
+            dsc_name => 'AD-Domain-Services',
+ 
+        }
+dsc_windowsfeature  {'addstools':
+            dsc_ensure => 'Present',
+            dsc_name => 'RSAT-ADDS',
+        }
   
   */
 
+  dsc_xaddomain   { 'primaryDC':
+    subscribe => Dsc_windowsfeature['addsinstall'],
+            dsc_domainname => 'ad.jsserv.local',
+    dsc_domainadministratorcredential => {
+              'user' => 'jsadmin',
+              'password' => Sensitive('Puppetlabs')
+    },
+    dsc_safemodeadministratorpassword   => {
+            'user' => 'jsadmin',
+              'password' => Sensitive('Puppetlabs')
+    },
+      dsc_databasepath => 'c:\NTDS',
+      dsc_logpath => 'c:\NTDS',
+  }
+
+  reboot {'dsc_reboot':
+      message => 'DSC has requested a reboot',
+      when => pending,
+  }
 }
