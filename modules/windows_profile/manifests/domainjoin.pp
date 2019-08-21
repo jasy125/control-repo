e03d1cc75b2ed49a58d148fb01e0a68199edbb37
jasy125 <jas89_44@hotmail.com>
2019-08-21 10:49:22 +0100

    domainjoin

--- modules/windows_profile/manifests/domainjoin.pp
+++ modules/windows_profile/manifests/domainjoin.pp
@@ -6,9 +6,8 @@ class windows_profile::domainjoin (
 ) {
 
     #Set Creds for creating the computer object
-    $creds = "New-Object System.Management.Automation.PSCredential( '${admin}', '${passw}' ); 
-    \ Add-Computer -DomainName ${domain} -OUPath ${machine_ou} -Restart -Force -Cred ${creds};"
-
+    $creds = "\$pscreds = New-Object System.Management.Automation.PSCredential( '${admin}', '${passw}' ); \
+     Add-Computer -DomainName ${domain} -OUPath ${machine_ou} -Restart -Force -Cred ${pscreds};"
 
   exec { 'join_domain':
 
