034d52a276b53ed943623c0a12ec9e0060d3ac2f
jasy125 <jas89_44@hotmail.com>
2019-08-21 10:59:13 +0100

    domainjoin

--- modules/windows_profile/manifests/domainjoin.pp
+++ modules/windows_profile/manifests/domainjoin.pp
@@ -6,31 +6,29 @@ class windows_profile::domainjoin (
 ) {
 
 #Set Creds for creating the computer object
-$code = " \
-\$secStr=ConvertTo-SecureString '${passw}' -AsPlainText -Force; \
-if (-not \$?) { \
-write-error 'Error: Unable to convert password string to a secure string'; \
-exit 10; \
-} \
-\$creds=New-Object System.Management.Automation.PSCredential( '${admin}', \$secStr ); \
-if (-not \$?) { \
-write-error 'Error: Unable to create PSCredential object'; \
-exit 20; \
-} \
-Add-Computer -DomainName ${domain} -OUPath ${machine_ou} -Restart -Force -Cred \$creds; \
-if (-not \$?) { \
-write-error 'Error: Unable to join domain'; \
-exit 30; \
-} \
-exit 0"
+  $code = " \
+  \$secStr=ConvertTo-SecureString '${passw}' -AsPlainText -Force; \
+  if (-not \$?) { \
+  write-error 'Error: Unable to convert password string to a secure string'; \
+  exit 10; \
+  } \
+  \$creds=New-Object System.Management.Automation.PSCredential( '${admin}', \$secStr ); \
+  if (-not \$?) { \
+  write-error 'Error: Unable to create PSCredential object'; \
+  exit 20; \
+  } \
+  Add-Computer -DomainName ${domain} -OUPath ${machine_ou} -Restart -Force -Cred \$creds; \
+  if (-not \$?) { \
+  write-error 'Error: Unable to join domain'; \
+  exit 30; \
+  } \
+  exit 0"
 
     
-
   exec { 'join_domain':
   command => $code,
   provider => powershell,
   logoutput => true,
   unless => "if ((Get-WMIObject Win32_ComputerSystem).Domain -ne '${domain}') { exit 1 }",
   }
-
 }
