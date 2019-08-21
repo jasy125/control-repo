8a48b7cfa3436d70b5099186f7929757f1ce28f8
jasy125 <jas89_44@hotmail.com>
2019-08-21 10:58:24 +0100

    domainjoin

--- modules/windows_profile/manifests/domainjoin.pp
+++ modules/windows_profile/manifests/domainjoin.pp
@@ -5,13 +5,29 @@ class windows_profile::domainjoin (
   $machine_ou = 'OU=Computers,DC=ad,DC=jsserv,DC=local',
 ) {
 
-    #Set Creds for creating the computer object
-    $creds = "\$pscreds = New-Object System.Management.Automation.PSCredential( '${admin}', '${passw}' ); \
-     Add-Computer -DomainName ${domain} -OUPath ${machine_ou} -Restart -Force -Cred ${pscreds};"
+#Set Creds for creating the computer object
+$code = " \
+\$secStr=ConvertTo-SecureString '${passw}' -AsPlainText -Force; \
+if (-not \$?) { \
+write-error 'Error: Unable to convert password string to a secure string'; \
+exit 10; \
+} \
+\$creds=New-Object System.Management.Automation.PSCredential( '${admin}', \$secStr ); \
+if (-not \$?) { \
+write-error 'Error: Unable to create PSCredential object'; \
+exit 20; \
+} \
+Add-Computer -DomainName ${domain} -OUPath ${machine_ou} -Restart -Force -Cred \$creds; \
+if (-not \$?) { \
+write-error 'Error: Unable to join domain'; \
+exit 30; \
+} \
+exit 0"
+
+    
 
   exec { 'join_domain':
-
-  command => $creds,
+  command => $code,
   provider => powershell,
   logoutput => true,
   unless => "if ((Get-WMIObject Win32_ComputerSystem).Domain -ne '${domain}') { exit 1 }",
