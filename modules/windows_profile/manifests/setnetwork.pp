691bb9a7c420ed654cedd119f40e3e7b2d840da4
jasy125 <jas89_44@hotmail.com>
2019-08-22 15:33:26 +0100

    setnetwork

---
+++ modules/windows_profile/manifests/setnetwork.pp
@@ -0,0 +1,41 @@
+class domain::setnetwork (
+
+  $ipadd = '10.234.1.209/8',
+  $defaultgateway = '192.168.0.1',
+  $dnsadd = '127.0.0.1, 8.8.8.8',
+  $dnsvalidate = true,
+  $firewall = { firewall => [
+              { firewall => 'domain',  set => false },
+              { firewall => 'public',  set => false },
+              { firewall => 'private', set => false }],
+              }
+
+) {
+
+  dsc_ipaddress {'setipaddress':
+    dsc_addressfamily  => 'IPV4',
+    dsc_interfacealias => 'Ethernet',
+    dsc_ipaddress      => $ipadd,
+  }
+
+  dsc_defaultgatewayaddress {'setdefaultgateway':
+    dsc_address        => $defaultgateway,
+    dsc_interfacealias => 'Ethernet',
+    dsc_addressfamily  => 'IPV4',
+  }
+
+  dsc_dnsserveraddress {'setdns':
+    dsc_address        => $dnsadd,
+    dsc_interfacealisa => 'Ethernet',
+    dsc_addressfamily  => 'IPV4',
+    dsc_validate       => $validate,
+  }
+
+  $firewall[firewall].each | $key | {
+    dsc_firewallprofile {"${key[firewall]}":
+      dsc_name    => "${key[firewall]}",
+      dsc_enabled => "${key[set]}",
+    }
+  }
+
+}
