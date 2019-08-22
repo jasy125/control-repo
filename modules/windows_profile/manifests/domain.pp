aded5c3f3bcc92550b31bfe56a7c26539db1ff4c
jasy125 <jas89_44@hotmail.com>
2019-08-22 11:39:17 +0100

    domainBuild

--- modules/windows_profile/manifests/domain.pp
+++ modules/windows_profile/manifests/domain.pp
@@ -7,6 +7,7 @@ class windows_profile::domain (
   $dclogpath = 'c:\NTDS',
   $forestmode = 'WinThreshold',
   $domainmode = 'WinThreshold',
+  $domaincontainer = "dc=jsserv,dc=local",
 
 ) {
 
@@ -25,28 +26,28 @@ class windows_profile::domain (
 
   dsc_windowsfeature  { 'addsinstall':
             dsc_ensure => 'Present',
-            dsc_name => 'AD-Domain-Services',
+            dsc_name   => 'AD-Domain-Services',
 
   }
   dsc_windowsfeature  {'addstools':
             dsc_ensure => 'Present',
-            dsc_name => 'RSAT-ADDS',
+            dsc_name   => 'RSAT-ADDS',
   }
   dsc_windowsfeature  {'DNS':
             dsc_ensure => 'Present',
-            dsc_name => 'DNS',
+            dsc_name   => 'DNS',
   }
 
   dsc_xaddomain   { 'primaryDC':
-    subscribe      => Dsc_windowsfeature['addsinstall'],
-    dsc_domainname => $dc,
-    dsc_domainnetbiosname => $dcnetbois,
+    subscribe                         => Dsc_windowsfeature['addsinstall'],
+    dsc_domainname                    => $dc,
+    dsc_domainnetbiosname             => $dcnetbois,
     dsc_domainadministratorcredential => $user,
     dsc_safemodeadministratorpassword => $passw,
-    dsc_forestmode => $forestmode,
-    dsc_domainmode => $domainmode,
-    dsc_databasepath => $dcdbpath,
-    dsc_logpath => $dclogpath,
+    dsc_forestmode                    => $forestmode,
+    dsc_domainmode                    => $domainmode,
+    dsc_databasepath                  => $dcdbpath,
+    dsc_logpath                       => $dclogpath,
 
     /*
     dsc_domainadministratorcredential => {
@@ -60,19 +61,35 @@ class windows_profile::domain (
     },
     */
   }
-  xWaitForADDomain 'WaitForDomainInstall' {
-      DomainName = $Node.DomainName;
-      DomainUserCredential = $DomainCredential;
-      RebootRetryCount = 2;
-      RetryCount = 10;
-      RetryIntervalSec = 60;
-      DependsOn = '[xADDomain]ADDomainInstall';
-    }
+  # Investigate building this into an array loop, build loop in order of ou top down ie layer one layer two based on layer one with key pair hash
+  dsc_xadorganizationalunit  {'CreateUKOU':
+      dsc_name => 'Uk',
+      dsc_path => $domaincontainer,
+      dsc_ensure => 'Present',
+      dsc_credential => $user,
+
+  }
+
+  dsc_xadorganizationalunit  {'CreateServersOU':
+      dsc_name => 'servers',
+      dsc_path => "OU=UK,${domaincontainer}",
+      dsc_ensure => 'Present',
+      dsc_credential => $user,
+      subscribe      => Dsc_xadorganizationalunit['CreateUKOU'],
+
+  }
+  dsc_xadorganizationalunit  {'CreateDesktopOU':
+      dsc_name => 'desktop',
+      dsc_path => "OU=UK,${domaincontainer}",
+      dsc_ensure => 'Present',
+      dsc_credential => $user,
+      subscribe      => Dsc_xadorganizationalunit['CreateUKOU'],
+  }
+
 
   # Lets create the OU needed for the computers.
   /*
-  dsc_xWaitForADDomain => {},
-  dsc_xADOrganizationalUnit => {},
+
    xWaitForADDomain 'WaitForDomainInstall' {
       DomainName = $Node.DomainName;
       DomainUserCredential = $DomainCredential;
