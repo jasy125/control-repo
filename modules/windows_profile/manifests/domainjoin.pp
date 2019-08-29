10f024f4c51cfbe9ee3ad537e97920e67d812b94
jasy125 <jas89_44@hotmail.com>
2019-08-29 13:39:25 +0100

    domainjoin

--- modules/windows_profile/manifests/domainjoin.pp
+++ modules/windows_profile/manifests/domainjoin.pp
@@ -6,29 +6,28 @@ class windows_profile::domainjoin (
   $machine_ou = "OU=windows,OU=puppet,DC=jsserv,DC=local",
 ) {
 
-#Set Creds for creating the computer object
-  $code = " \
-  \$secStr=ConvertTo-SecureString '${passw}' -AsPlainText -Force; \
-  if (-not \$?) { \
-  write-error 'Error: Unable to convert password string to a secure string'; \
-  exit 10; \
-  } \
-  \$creds=New-Object System.Management.Automation.PSCredential( '${admin}', \$secStr ); \
-  if (-not \$?) { \
-  write-error 'Error: Unable to create PSCredential object'; \
-  exit 20; \
-  } \
-  Add-Computer -DomainName '${domain}' -OUPath '${machine_ou}' -Restart -Force -Cred \$creds; \
-  if (-not \$?) { \
-  write-error 'Error: Unable to join domain'; \
-  exit 30; \
-  } \
-  exit 0"
-    
-  exec { 'join_domain':
-  command => $code,
-  provider => powershell,
-  logoutput => true,
-  unless => "if ((Get-WMIObject Win32_ComputerSystem).Domain -ne ${domain}) { exit 1 }",
+#Set network default gateway to ip of the domain server first.
+dsc_dnsserveraddress { 'dnsserveraddress':
+    dsc_address        => $dnsserverip,
+    dsc_interfacealias => 'Ethernet',
+    dsc_addressfamily  => 'IPv4',
+  }
+
+
+  dsc_computer { 'joindomain':
+    dsc_name        => $compname,
+    dsc_domainname  => $domain,
+    dsc_joinou      => $machine_ou,
+    dsc_description => "Newly Joined ${compname}",
+    dsc_credential  => {
+      user     => $admin,
+      password => Sensitive($passw)
+    },
+    subscribe       => Dsc_dnsserveraddress['dnsserveraddress'],
+  }
+
+  reboot {'dsc_reboot':
+      message => 'DSC has requested a reboot',
+      when    => pending,
   }
 }
