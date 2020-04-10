4cd70a3411bf7999dbf19eba380494b96e7062c3
root <admin@puppet.com>
2020-04-10 10:49:00 +0100

    scheduledtask

--- modules/windows_profile/manifests/scheduledtask.pp
+++ modules/windows_profile/manifests/scheduledtask.pp
@@ -1,7 +1,7 @@
 class windows_profile::scheduledtask (
   Hash $scheduledtask = {},
   ) {
-
+      notify{"Create Task" :}
       $scheduledtask.each | $_k, $_v | {
         dsc_scheduledtask { $_k:
           * => $_v,
