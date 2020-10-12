cc0bd838ec7214c0f2ac5c94881f60eecc818ee4
root <admin@puppet.com>
2020-10-12 17:21:29 +0100

    sudomod

--- modules/linux_profile/manifests/test.pp
+++ modules/linux_profile/manifests/test.pp
@@ -4,4 +4,9 @@ class linux_profile::test (
 $mytest = "$linux_profile::test"
 
   notify{ "$mytest" :}
+
+  sudo::conf { 'joe':
+  priority => 60,
+  source   => 'puppet:///files/etc/sudoers.d/users/joe',
+  }
 }
